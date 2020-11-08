f7cad0b21ec2feec2419ab7b831a3df81da6b8f8
vsevolodglazov <gla-seva@yandex.ru>
2020-11-08 14:50:33 +0300

    Added fit_transform to TfIdfTransformer

--- tfidf_vectorizer.py
+++ tfidf_vectorizer.py
@@ -1,20 +1,17 @@
 from count_vectorizer import CountVectorizer
 from tfidf_transformer import TfidfTransformer
-from typing import List, Iterable
+from typing import Iterable, List
 
 
-class TfidfVectorizer(CountVectorizer, TfidfTransformer):
+class TfidfVectorizer(CountVectorizer):
     def __init__(self, lowercase: bool = True, token_pattern: str = r"(?u)\b\w\w+\b"):
         super().__init__(lowercase, token_pattern)
+        self._tfidf = TfidfTransformer()
 
     def transform(self, x: Iterable) -> List[List[float]]:
         """
-        Строит матрицу df-idf
+        Трансформер последовательно применяет сначала из CountVectorizer затем TfidfTransformer
         """
         count_matrix = super().transform(x)
-        tf_matrix = self.tf_transform(count_matrix)
-        idf_vector = self.idf_transform(count_matrix)
-
-        tfidf_matrix = [[tf * idf for tf, idf in zip(tf_vector, idf_vector)]
-                        for tf_vector in tf_matrix]
+        tfidf_matrix = self._tfidf.fit_transform(count_matrix)
         return tfidf_matrix
